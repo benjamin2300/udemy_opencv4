81e72f1e29c7e59e442311b85e55416ca33c62c8
benjamin2300 <benjamin2300.b98902064@gmail.com>
2020-01-21 23:06:47 +0800

    manipulation 1 : transition

---
+++ opencv_image_manipulation/opencv_manipulation1_image_manipulation.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('images/shiki.jpg')
+
+height, width = image.shape[:2]
+quater_height = height / 4
+quater_width = width / 4
+# translation matrix, 2 x 3 matrix
+T = np.float32([[1, 0, quater_width], [0, 1, quater_height]])
+# cv2.wrapAffine(image, T matrix, (width, height))
+img_transition = cv2.warpAffine(image, T, (width, height))
+cv2.imshow("Image Transition", img_transition)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
